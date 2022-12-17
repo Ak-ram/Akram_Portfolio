7c862b3a8243386d171432a89713f3d996122fd2
Restyled.io <commits@restyled.io>
2022-12-17 21:53:23 +0000

    Restyled by whitespace

--- src/Components/GitHubRepos.tsx
+++ src/Components/GitHubRepos.tsx
@@ -135,8 +135,8 @@ export default function Repos() {
           </div>
           <div className="px-3 py-2 flex flex-col">
             <p>This Repo will permenantly delete</p>
-            <button className="mt-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm px-2 py-1 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" 
-            data-reponame={name} 
+            <button className="mt-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm px-2 py-1 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            data-reponame={name}
             onClick={(event) => {
               deleteRepo(event)
               setIsDeleted(true)
