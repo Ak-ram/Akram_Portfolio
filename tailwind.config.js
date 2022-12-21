109d3a51fbba871328260473bbf6819c1359846c
Restyled.io <commits@restyled.io>
2022-12-21 12:41:44 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors:{
-      "card":"#171717",
-      "white": "#ffffff",
+  content : [ "./src/**/*.{js,jsx,ts,tsx}" ],
+  darkMode : "class",
+  theme : {
+    extend : {
+      colors : {
+        "card" : "#171717",
+        "white" : "#ffffff",
+      },
+      screens : {
+        xs : "450px",
+      }
     },
-    screens:{
-       xs: "450px",
-    }
-    },
-    screens: {
-      ...defaultTheme.screens
-    },
-     
+    screens : {...defaultTheme.screens},
+
   },
-  plugins: [],
+  plugins : [],
 };
