6ac8703f2fd2e192c8914a0b2c0031665afabae9
Restyled.io <commits@restyled.io>
2022-12-22 19:04:13 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content : [ "./src/**/*.{js,jsx,ts,tsx}" ],
-  darkMode : "class",
-  theme : {
-    extend : {
-      colors : {
-        "card" : "#171717",
-        "white" : "#ffffff",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        card: "#171717",
+        white: "#ffffff",
       },
-      screens : {
-        xs : "450px",
+      screens: {
+        xs: "450px",
       },
-      animation : {
-        'ping-once' : 'ping .2s linear 1',
-      }
+      animation: {
+        "ping-once": "ping .2s linear 1",
+      },
     },
-    screens : {...defaultTheme.screens},
-
+    screens: { ...defaultTheme.screens },
   },
-  plugins : [],
+  plugins: [],
 };
