50394babd423c9f27075ea1de5eace2f3fb44497
Restyled.io <commits@restyled.io>
2022-12-28 16:49:33 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        card: "#171717",
-        white: "#ffffff",
+  content : [ "./src/**/*.{js,jsx,ts,tsx}" ],
+  darkMode : "class",
+  theme : {
+    extend : {
+      colors : {
+        card : "#171717",
+        white : "#ffffff",
       },
-      screens: {
-        xs: "450px",
+      screens : {
+        xs : "450px",
       },
-      keyframes: {
-        move: {
-           "0%": { transform: "translatey(55px)", opacity: 1 },
-          "100%": { transform: "translatey(-108px)" , opacity: 0 },
+      keyframes : {
+        move : {
+          "0%" : {transform : "translatey(55px)", opacity : 1},
+          "100%" : {transform : "translatey(-108px)", opacity : 0},
         },
       },
-      animation: {
-        "ping-once": "ping .2s linear 1",
-        "move-vertical": "move 5s linear infinite alternate both",
+      animation : {
+        "ping-once" : "ping .2s linear 1",
+        "move-vertical" : "move 5s linear infinite alternate both",
       },
     },
-    screens: {
+    screens : {
       ...defaultTheme.screens,
     },
   },
-  plugins: [],
+  plugins : [],
 };
